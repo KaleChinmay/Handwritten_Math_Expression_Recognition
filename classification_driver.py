cc57835bccad92ee0daf66506310f9b69f024243
KaleChinmay <43867658+KaleChinmay@users.noreply.github.com>
2020-03-09 00:34:43 -0400

    new commit

--- classification_driver.py
+++ classification_driver.py
@@ -13,6 +13,8 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.neural_network import MLPClassifier
 import preprocessing
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -86,6 +88,26 @@ features_dict = {
 
 }
 
+class_label_dict = {}
+THRESHOLD = 10
+
+
+
+def preprocess(data, train_labels):
+	elements, count = np.unique(train_labels, return_counts =True)
+	for key, value in zip(elements, count):
+		class_label_dict[key] = value
+
+	below_threshold_labels = []
+	for key in class_label_dict.keys():
+		if(class_label_dict[key]<THRESHOLD):
+			below_threshold_labels.append(key)
+	data = data[~data['Class label'].isin(below_threshold_labels)]
+
+	return data
+
+
+
 
 def classify(data, classifier_param, junk_param, train_param, testing_data=None):
 	if(junk_param=='2'):
@@ -98,19 +120,22 @@ def classify(data, classifier_param, junk_param, train_param, testing_data=None)
 		testing_data = testing_data.drop(columns=['Class label'], axis=1)
 		data = data.fillna(0)
 		data = data.replace([np.inf, -np.inf], 0)
+		data = preprocess(data, data['Class label'])
+		le = pp.LabelEncoder()
+		train_labels = le.fit_transform(data['Class label'])
 		features = data.drop(columns=['Class label'], axis=1)
 		if(train_param=='1'):
-			labels = np.array(data['Class label'])
-			le = pp.LabelEncoder()
-			train_labels = le.fit_transform(data['Class label'])
+			#labels = np.array(data['Class label'])
 			if classifier_param == '0':
 				print('Using KDTree')
 				classifier = KNeighborsClassifier(n_neighbors=1,algorithm='kd_tree')
 			elif(classifier_param == '1'):
 				print('Using Ensemble')
+				classifier = RandomForestClassifier(n_estimators=150, random_state=42, max_depth=35)
+				#classifier = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(100, 5), random_state=42)
 				#model1 = LogisticRegression(random_state=1)
 				#model2 = tree.DecisionTreeClassifier(random_state=1)
-				classifier = RandomForestClassifier(n_estimators=150, random_state=42)
+				
 
 				#classifier = VotingClassifier(estimators=[('lr', model1), ('rf', model3)], voting='hard')
 				#classifier = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -202,80 +227,6 @@ def classify(data, classifier_param, junk_param, train_param, testing_data=None)
 		print('Generated Ground Truth and Output files')
 		return prediction_file, GT_file
 
-
-
-
-'''
-def classify(data, classifier_param, junk_param, train_param):
-	classifier = None
-	classifier_file = ''
-	classifier_file = 'model_'+classifier_type_map[classifier_param]+"_"+data_type_map[junk_param]+'.txt'
-	model = None
-	data = data.fillna(0)
-	data = data.replace([np.inf, -np.inf], 0)
-	features = data.drop(columns=['Class label'], axis=1)
-	labels = np.array(data['Class label'])
-	le = pp.LabelEncoder()
-	encoded_labels = le.fit_transform(data['Class label'])
-
-	train_features, test_features, train_labels, test_labels = train_test_split(features, encoded_labels,
-		test_size = 0.3, random_state = 42, stratify=encoded_labels)
-	print(train_param)
-	
-	if(train_param == '0'):
-		if(path.exists(classifier_file)):
-			print('Found existing model :',classifier_file)
-			model_info = joblib.load(classifier_file)
-			model = model_info['model']
-			le = model_info['char_encoder']
-			print('Using model ',classifier_file)
-		else:
-			print('Model '+classifier_file+' not found. Returning')
-			return None, None
-	else:
-		if classifier_param == '0':
-			print('Using KDTree')
-			classifier = KNeighborsClassifier(n_neighbors=1,algorithm='kd_tree')
-		elif(classifier_param == '1'):
-			print('Using RandomForest')
-			classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-		else:
-			print('Invalid input, returning')
-			return None, None
-		model = classifier.fit(train_features, train_labels)
-		classifier_file = 'model_'+classifier_type_map[classifier_param]+"_"+data_type_map[junk_param]+'.txt'
-		model_info = {'model':model, 'char_encoder':le}
-		with open(classifier_file,'wb') as f:
-			joblib.dump(model_info, f, compress=3)
-		print('Using model ',classifier_file)
-
-
-	predictions = model.predict(test_features)
-	GT_file = None
-	if (junk_param !='2'):
-		accuracy = accuracy_score(predictions, test_labels)
-
-		
-		test_labels = le.inverse_transform(test_labels)
-		print(accuracy)
-		#Write GT (input file)
-		test_features['Class label'] = test_labels
-		gt_data = test_features['Class label']
-		GT_file = classifier_type_map[classifier_param]+'_'+data_type_map[junk_param]+'_GT.csv'
-		write_csv.write_output_files(gt_data, GT_file)
-		print('Created file : '+GT_file)
-	#Write Prediction (output file)
-
-	predictions = le.inverse_transform(predictions)
-	test_features['predictions'] = predictions
-	ouput_data = test_features['predictions']
-	prediction_file = classifier_type_map[classifier_param]+'_'+data_type_map[junk_param]+'_prediction.csv'
-	write_csv.write_output_files(ouput_data, prediction_file)
-	print('Created file : '+prediction_file)
-	print('Generated Ground Truth and Output files')
-
-	return prediction_file, GT_file
-'''
 
 
 def classification(junk_param, classifier_param, train_param):
